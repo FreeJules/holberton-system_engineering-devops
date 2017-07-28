1c4d7d336d0dfc9b66dfd2ce0d223ecd25a987f0
Julija Lee <lee.julija@gmail.com>
2017-07-28 01:50:31 +0000

    2-execute_a_command.pp

---
+++ 0x0F-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow
+exec { 'killmenow':
+  onlyif  => 'pgrep -f killmenow',
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin:/usr/sbin'
+}
