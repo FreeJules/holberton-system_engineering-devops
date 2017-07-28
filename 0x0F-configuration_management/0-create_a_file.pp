7b1d1b277a7a75bb3dca7d9ed7606fc32350f57e
Julija Lee <lee.julija@gmail.com>
2017-07-28 00:45:15 +0000

    fixed typos

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #  create a file in /tmp
 file { '/tmp/holberton':
-  ensure     => file,
-  path       => '/tmp/holberton',
-  permission => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  contains   => 'I love Puppet',
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
