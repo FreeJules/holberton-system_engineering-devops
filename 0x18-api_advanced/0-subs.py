fd4b9554b5c4d4ce8856e3c423406d95675262f3
Julija Lee <lee.julija@gmail.com>
2017-09-22 00:26:42 +0000

    initial add

---
+++ 0x18-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+returns the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers
+    """
+    url = "https://www.reddit.com/r/" + subreddit + "/about/.json"
+    r = requests.get(url, headers={'User-agent': 'holberton 0.1'})
+    if not r:
+        return 0
+    json = r.json()
+    data = json.get("data")
+    subscribers = data.get("subscribers")
+    if not subscribers:
+        return 0
+    return subscribers
